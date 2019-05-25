48aecbc150611d08cbc3046f00e6cbc6780410b1
Toshiaki Maki <tmaki@pivotal.io>
2019-05-25 18:30:11 +0900

    Stop using CURRENT_DATE as a default value of date values

--- src/main/java/am/ik/handson/App.java
+++ src/main/java/am/ik/handson/App.java
@@ -96,19 +96,19 @@ public class App {
         if ("H2".equals(name)) {
             databaseClient.execute()
                 .sql("CREATE TABLE IF NOT EXISTS expenditure (expenditure_id INT PRIMARY KEY AUTO_INCREMENT, expenditure_name VARCHAR(255), unit_price INT NOT NULL, quantity INT NOT NULL, " +
-                    "expenditure_date DATE NOT NULL DEFAULT CURRENT_DATE)")
+                    "expenditure_date DATE NOT NULL)")
                 .then()
                 .then(databaseClient.execute()
-                    .sql("CREATE TABLE IF NOT EXISTS income (income_id INT PRIMARY KEY AUTO_INCREMENT, income_name VARCHAR(255), amount INT NOT NULL, income_date DATE NOT NULL DEFAULT CURRENT_DATE)")
+                    .sql("CREATE TABLE IF NOT EXISTS income (income_id INT PRIMARY KEY AUTO_INCREMENT, income_name VARCHAR(255), amount INT NOT NULL, income_date DATE NOT NULL)")
                     .then())
                 .subscribe();
         } else if ("PostgreSQL".equals(name)) {
             databaseClient.execute()
                 .sql("CREATE TABLE IF NOT EXISTS expenditure (expenditure_id SERIAL PRIMARY KEY, expenditure_name VARCHAR(255), unit_price INT NOT NULL, quantity INT NOT NULL, " +
-                    "expenditure_date DATE NOT NULL DEFAULT CURRENT_DATE)")
+                    "expenditure_date DATE NOT NULL)")
                 .then()
                 .then(databaseClient.execute()
-                    .sql("CREATE TABLE IF NOT EXISTS income (income_id SERIAL PRIMARY KEY, income_name VARCHAR(255), amount INT NOT NULL, income_date DATE NOT NULL DEFAULT CURRENT_DATE)")
+                    .sql("CREATE TABLE IF NOT EXISTS income (income_id SERIAL PRIMARY KEY, income_name VARCHAR(255), amount INT NOT NULL, income_date DATE NOT NULL)")
                     .then())
                 .subscribe();
         }
