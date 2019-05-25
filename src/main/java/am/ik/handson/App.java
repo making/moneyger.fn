48c46e584b26e43cad75599968780f525ec70c1e
Toshiaki Maki <tmaki@pivotal.io>
2019-05-25 10:02:29 +0900

    Use file: instead of mem:

--- src/main/java/am/ik/handson/App.java
+++ src/main/java/am/ik/handson/App.java
@@ -77,7 +77,7 @@ public class App {
     }
 
     static ConnectionFactory connectionFactory() {
-        return ConnectionFactories.get("r2dbc:h2:mem:///test?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        return ConnectionFactories.get("r2dbc:h2:file:///./target/demo?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
     }
 
     static void initializeDatabase(String name, DatabaseClient databaseClient) {
