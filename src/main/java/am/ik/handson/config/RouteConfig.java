657d7ceeb11d6cfb801be6ee5fa1d626c8151bd8
Toshiaki Maki <tmaki@pivotal.io>
2019-09-01 18:51:52 +0900

    Remove static file routing

--- src/main/java/am/ik/handson/config/RouteConfig.java
+++ src/main/java/am/ik/handson/config/RouteConfig.java
@@ -4,34 +4,15 @@ import am.ik.handson.expenditure.ExpenditureHandler;
 import am.ik.handson.income.IncomeHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.CacheControl;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-
-import java.time.Duration;
 
 @Configuration
 public class RouteConfig {
 
     @Bean
     public RouterFunction<ServerResponse> routes(ExpenditureHandler expenditureHandler, IncomeHandler incomeHandler) {
-        return staticRoutes()
-            .and(expenditureHandler.routes())
+        return expenditureHandler.routes()
             .and(incomeHandler.routes());
-    }
-
-    static RouterFunction<ServerResponse> staticRoutes() {
-        return RouterFunctions.route()
-            .GET("/", req -> ServerResponse.ok().bodyValue(new ClassPathResource("META-INF/resources/index.html")))
-            .resources("/docs/**", new ClassPathResource("static/docs/"))
-            .resources("/webjars/**", new ClassPathResource("META-INF/resources/webjars/"))
-            .resources("/**", new ClassPathResource("META-INF/resources/"))
-            .filter((request, next) -> next.handle(request)
-                .flatMap(response -> ServerResponse.from(response)
-                    .cacheControl(CacheControl.maxAge(Duration.ofDays(3)))
-                    .build(response::writeTo)))
-            .build();
     }
 }
