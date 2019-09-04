5990467f0c6fdedbec60bb6d3018987cf1446ce0
Toshiaki Maki <tmaki@pivotal.io>
2019-09-05 04:01:50 +0900

    Support native-image

---
+++ build-native-image-for-linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker run --rm \
+           -v "$PWD":/usr/src \
+           -v "$HOME/.m2":/root/.m2 \
+           -w /usr/src \
+           oracle/graalvm-ce:19.2.0 \
+           ./mvnw clean package -Pgraal -DskipTests=true $@
